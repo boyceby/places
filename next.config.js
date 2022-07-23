df6c61c635601b63844b9d6b1e8e47375af16b85
Boyce Young <boycebyoung@gmail.com>
2022-07-22 22:54:14 -0400

    Update next.config.js to include module.exports statement

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["maps.googleapis.com"],
+    domains: ["maps.googleapis.com", ""],
   },
 };
+
+module.exports = nextConfig;
 
 // DEV CONFIGURATION:
 // const webpack = require("webpack");
